585b5ff386f4bbafd193c96c9d9cebc3e1ca44af
SPROYoYo <43702866+SPROYoYo@users.noreply.github.com>
2020-07-07 01:13:50 +0300

    Update example.php

--- example.php
+++ example.php
@@ -1,9 +1,9 @@
 <?php
 include 'EasyUpload.php';
 
-EasyUpload::$destination = 'uploads/images; # path
-EasyUpload::$extensions = ['.jpg', '.jpeg', '.png', '.bmp']; # allowed extensions list
-EasyUpload::$maxMemory = 2.5; # set max file size to be allowed in MB
+EasyUpload::$destination = 'uploads/images; /* path */
+EasyUpload::$extensions = ['.jpg', '.jpeg', '.png', '.bmp']; /* allowed extensions list */
+EasyUpload::$maxMemory = 2.5; /* set max file size to be allowed in MB */
 EasyUpload::$resize = [
 	["uploads/images-200x200", 200, 200],
 	["uploads/images-300x300", 300, 300],
